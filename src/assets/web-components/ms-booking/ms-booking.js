e54496a10ad8b0e11c77018ed32eb12d7bed0f83
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-04-10 12:26:43 +0530

    - Imported msAppend - Added getCaptcha function - Fixed error which was throwing errors by making the element try to iterate buttons which do not exist - Added function to disconnected callback which removes event listeners from buttons - Edited confirmInput function to get the captcha object which is returned from the getCaptcha function - Added a disabled attribute to the elements which are created for the confirmation page - Added call to update props in order to add the captcha data - Appended the captcha field to the form - Appended the data from the props object for the captcha test to the body of the data sent to the mail server for verification - Changed the fetch call to use the href set in the action parameter of the ms-booking element - Also added the method which is specified

--- src/assets/web-components/ms-booking/ms-booking.js
+++ src/assets/web-components/ms-booking/ms-booking.js
@@ -3,7 +3,8 @@ import slideSCSS from './sass/slide.ce.scss';
 import {
     msCreate,
     msQuery,
-    msQueryAll
+    msQueryAll,
+    msAppend
 } from 'making-stuffs-queries';
 
 class MsBooking extends HTMLElement {
@@ -58,6 +59,7 @@ class MsBookingSlide extends MsBooking {
         this.updateState = this.updateState.bind(this);
         this.confirmInput = this.confirmInput.bind(this);
         this.boundSubmitHandler = this.submitHandler.bind(this);
+        this.getCaptcha = this.getCaptcha.bind(this);
     }
 
     static get observedAttributes() {
@@ -89,7 +91,9 @@ class MsBookingSlide extends MsBooking {
             this.shadowRoot.appendChild(style);
             this.wrapper = msQuery('.wrapper', this);
             this.shadowRoot.appendChild(this.wrapper);
+
             const buttons = msQueryAll('button', this.shadowRoot);
+            if (!buttons) return;
             buttons.forEach(button => {
                 button.addEventListener('click', this.boundClickHandler);
             });
@@ -97,7 +101,10 @@ class MsBookingSlide extends MsBooking {
     }
 
     disconnectedCallback() {
-        this.console.log('bye');
+        const buttons = msQueryAll('button', this.shadowRoot);
+        buttons.forEach(button => {
+            button.removeEventListener('click', this.boundClickHandler);
+        });
     }
 
     clickHandler(e) {
@@ -148,17 +155,23 @@ class MsBookingSlide extends MsBooking {
 
     confirmInput(nextSlide) {
         const state = super.props();
+
         const form = msCreate('form', {
             class: 'confirm-container',
             action: state.action,
             method: state.method,
             enctype: state.enctype
         });
+
         const submit = msCreate('input', {
             type: 'submit',
             class: 'button'
         });
+
+        const captcha = this.getCaptcha(state);
+
         const nextSlideWrapper = msQuery('.wrapper', nextSlide.shadowRoot);
+
         for (let prop in state) {
 
             if (prop === 'action' || prop === 'method' || prop === 'enctype') continue;
@@ -166,31 +179,103 @@ class MsBookingSlide extends MsBooking {
             const row = msCreate('div', {
                 class: 'confirm-row'
             });
+
             const fieldName = (prop.split(/(?=[A-Z])/g)).join(' ');
+
             row.innerHTML = `
-                <label>${fieldName}:</label> <input value="${state[prop]}" name="${prop}" />
+                <label>${fieldName}:</label> <input disabled value="${state[prop]}" name="${prop}" />
             `;
+
             form.appendChild(row);
         }
-        form.appendChild(submit);
-        nextSlideWrapper.appendChild(form);
+
+        msAppend([captcha.elem, submit], form);
+
+        msAppend(form, nextSlideWrapper);
+
+        super.updateProps({
+            captcha: {
+                index: captcha.index,
+                letter: captcha.letter,
+                field: captcha.field
+            }
+        });
+
         form.addEventListener('submit', this.boundSubmitHandler);
+    }
+
+    getCaptcha(state) {
+
+        const totalFields = Object.keys(state);
+
+        const fieldIndex = Math.floor(Math.random() * Math.floor(totalFields.length));
+
+        const testField = totalFields[fieldIndex];
+
+        if (testField === 'phone' ||
+        testField === 'headCount' || 
+        testField === 'time' || 
+        testField === 'date' || 
+        testField === 'enctype' || 
+        testField === 'method' || 
+        testField === 'action') return this.getCaptcha(state);
+
+        const getCaptchaObject = () => {
+            let captchaObject = {};
+            let testValue = state[testField].includes('@') ? state[testField].split('@')[0] : state[testField];
+            const testIndex = Math.floor(Math.random() * Math.floor(testValue.length));
+            captchaObject.letter = state[testField][testIndex];
+            captchaObject.index = testIndex;
+            captchaObject.field = testField;
+            return captchaObject;
+        };
+
+        const captchaObject = getCaptchaObject();
+
+        const fieldTitle = testField !== 'email' ? testField.split(/(?=[A-Z])/g).join(' ').toLowerCase() : testField.toLowerCase();
+
+        const captchaRow = msCreate('div', {
+            class: 'confirm-row captcha'
+        });
+
+        const displayCaptchaIndex = captchaObject.index + 1;
+
+        const captchaLabel = msCreate('label', {
+            class: 'captcha-label',
+            innerHTML: `Prove you're human:
+            <span>Please enter the ${displayCaptchaIndex === 1 ? displayCaptchaIndex + 'st' : displayCaptchaIndex === 2 ? displayCaptchaIndex + 'nd' : displayCaptchaIndex === 3 ? displayCaptchaIndex + 'rd' : displayCaptchaIndex + 'th'} letter of your ${fieldTitle}</span>`
+        });
+
+        const captcha = msCreate('input', {
+            name: 'captcha',
+            placeholder: 'CAPTCHA'
+        });
+
+        msAppend([captchaLabel, captcha], captchaRow);
+
+        captchaObject.elem = captchaRow;
+
+        return captchaObject;
     }
 
     async submitHandler(e) {
         e.preventDefault();
         const fields = e.currentTarget.elements;
         let body = {};
+        const state = super.props();
+        console.log(state.captcha);
+        body.captchaCheck = state.captcha;
 
-        for(let field of fields){
-            if(field.type === 'submit') continue;
+        for (let field of fields) {
+            if (field.type === 'submit') continue;
             body[field.name] = field.value;
         }
 
-        const reply = await fetch('https://localhost:60002/auth', {
+        const reply = await fetch(state.action, {
             headers: {
                 'Content-Type': 'application/json'
             },
+            method: state.method,
             body: JSON.stringify(body)
         });
 
